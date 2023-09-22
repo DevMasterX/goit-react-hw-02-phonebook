0bb1456a9aa54828d3b65baa8dcffbd28bd6fb14
DevMasterX <sokol080288@gmail.com>
2023-09-23 02:03:47 +0300

    step 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,17 @@ import { Component } from 'react';
 import { nanoid } from 'nanoid';
 export class App extends Component {
   state = {
-    contacts: ['Rosie Simpson', 'Hermione Kline', 'Eden Clements'],
+    contacts: [],
     name: '',
+    number: '',
   };
 
-  handleInputChange = event => {
-    this.setState({ name: event.target.value });
+  // handleInputChange = event => {
+  //   this.setState({ name: event.target.value });
+  // };
+
+  handleInputChange = ({ target: { value, name } }) => {
+    this.setState({ [name]: value });
   };
 
   handleFormSubmit = event => {
@@ -16,30 +21,46 @@ export class App extends Component {
     const newContact = {
       id: nanoid(),
       name: this.state.name,
+      number: this.state.number,
     };
 
-    this.setState({ contacts: [...this.state.contacts, newContact], name: '' });
+    this.setState({
+      contacts: [...this.state.contacts, newContact],
+      name: '',
+      number: '',
+    });
   };
-
-  // createContact=(dataByForm)=>{
-  //   id: nanoid()
-  // }
 
   render() {
     return (
       <>
         <form onSubmit={this.handleFormSubmit}>
           <div>
-            <label htmlFor="exampleInputTodo">Name</label>
-            <input
-              type="text"
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-              value={this.state.name}
-              onChange={this.handleInputChange}
-            />
+            <label>
+              Name
+              <input
+                type="text"
+                name="name"
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                required
+                value={this.state.name}
+                onChange={this.handleInputChange}
+              />
+            </label>
+
+            <label>
+              Number
+              <input
+                type="tel"
+                name="number"
+                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                required
+                value={this.state.number}
+                onChange={this.handleInputChange}
+              />
+            </label>
           </div>
 
           <button type="submit">Add contact</button>
@@ -47,7 +68,9 @@ export class App extends Component {
 
         <ul>
           {this.state.contacts.map(contact => (
-            <li key={contact.id}>{contact.name}</li>
+            <li key={contact.id}>
+              {contact.name}: {contact.number}
+            </li>
           ))}
         </ul>
       </>
